733dfbbd4dee6dcc4df2730ce55a7e8334870910
Joseph Lambert <josephc.lambert@gmail.com>
2019-11-06 14:40:51 -0800

    Display the order Id and shipper CompanyName for all orders placed before August 9 2012. Shows 429 records.

--- queries.sql
+++ queries.sql
@@ -1,12 +1,13 @@
 -- Multi-Table Query Practice
 
 -- Display the ProductName and CategoryName for all products in the database. Shows 77 records.
-select p.ProductName, c.CategoryName
-from Product as p
-join Category as c
-on p.CategoryId=c.Id;
+-- select p.ProductName, c.CategoryName
+-- from Product as p
+-- join Category as c
+-- on p.CategoryId=c.Id;
 -- Display the order Id and shipper CompanyName for all orders placed before August 9 2012. Shows 429 records.
-
+select Order.Id, Shipper.CompanyName
+from [Order];
 -- Display the name and quantity of the products ordered in order with Id 10251. Sort by ProductName. Shows 3 records.
 
 -- Display the OrderID, Customer's Company Name and the employee's LastName for every order. All columns should be labeled clearly. Displays 16,789 records.
