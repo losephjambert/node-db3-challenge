fd406ae60019bc708dd04ae3edb05074af8247a4
Joseph Lambert <josephc.lambert@gmail.com>
2019-11-06 15:13:55 -0800

    Display the name and quantity of the products ordered in order with Id 10251. Sort by ProductName. Shows 3 records.

--- queries.sql
+++ queries.sql
@@ -5,9 +5,22 @@
 -- from Product as p
 -- join Category as c
 -- on p.CategoryId=c.Id;
+
 -- Display the order Id and shipper CompanyName for all orders placed before August 9 2012. Shows 429 records.
-select Order.Id, Shipper.CompanyName
-from [Order];
+-- select o.Id, s.CompanyName
+-- from [Order] as o
+-- join Shipper as s
+-- on o.ShipVia=s.Id
+-- where o.OrderDate < '2012-08-09';
+
 -- Display the name and quantity of the products ordered in order with Id 10251. Sort by ProductName. Shows 3 records.
+-- select p.ProductName, od.Quantity
+-- from OrderDetail as od
+-- join Product as p
+-- on p.Id = od.ProductId
+-- where od.OrderId=10251
+-- order by p.ProductName;
+
+-- select * from Product as p where p.Id=22;
 
 -- Display the OrderID, Customer's Company Name and the employee's LastName for every order. All columns should be labeled clearly. Displays 16,789 records.
